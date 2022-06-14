e2d9fa3c48cd21e37cfc6ed1ca94065afb9f4514
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-06-14 14:52:26 +0300

    route placeholders added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+// const appRouter = require('./appRoutes');
 
 const app = express();
 const { PORT } = process.env;
@@ -9,5 +10,9 @@ const { PORT } = process.env;
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.get('/list', (req, res) => res.json('list'));
+app.get('/scan', (req, res) => res.json('scan'));
+app.get('/download-state', (req, res) => res.json('state'));
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
